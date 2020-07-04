76a3cf9037439a2a96b855d4aaf00ee8c0aabe3b
sally4405 <sally4405@naver.com>
2020-07-04 12:30:21 +0900

    refactor: 1002 입력 처리 수정

--- 1002.js
+++ 1002.js
@@ -2,26 +2,34 @@
 // 터렛
 
 // const input = [3, 0, 0, 13, 40, 0, 37, 0, 0, 3, 0, 7, 4, 1, 1, 1, 1, 1, 5];
-const input = [8, 0, 0, 1, 0, 4, 5, 0, 0, 1, 0, 4, 3, 0, 0, 1, 0, 4, 2, 0, 0, 1, 0, 4, 6, 0, 0, 1, 0, 4, 5];
+// const input = [8, 0, 0, 1, 0, 4, 5, 0, 0, 1, 0, 4, 3, 0, 0, 1, 0, 4, 2, 0, 0, 1, 0, 4, 6, 0, 0, 1, 0, 4, 5];
 // const input = [1, 1, 1, 1, 1, 1, 5];
-// const input = require("fs")
+// const input = [2, 0, 0, 1, 0, 0, 1, 0, 0, 10, 0, 0, 1];
+const raw = "3 \n0 0 13 40 0 37 \n0 0 3 0 7 4 \n1 1 1 1 1 5";
+const inputArray = raw
+  .toString()
+  .split("\n")
+  .map((n) => n.split(" "));
+// const inputArray = require("fs")
 //   .readFileSync("/dev/stdin")
 //   .toString()
 //   .split("\n")
-//   .map((n) => parseInt(n));
+//   .map((n) => n.split(" "));
+
+console.log(inputArray);
 
 const getDistance = (x1, y1, x2, y2) => {
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
 
-const testCase = input[0];
-for (let i = 0; i < testCase; i++) {
-  const x1 = input[i * 6 + 1];
-  const y1 = input[i * 6 + 2];
-  const r1 = input[i * 6 + 3];
-  const x2 = input[i * 6 + 4];
-  const y2 = input[i * 6 + 5];
-  const r2 = input[i * 6 + 6];
+const testCase = inputArray[0][0];
+for (let i = 1; i <= testCase; i++) {
+  const x1 = parseInt(inputArray[i][0]);
+  const y1 = parseInt(inputArray[i][1]);
+  const r1 = parseInt(inputArray[i][2]);
+  const x2 = parseInt(inputArray[i][3]);
+  const y2 = parseInt(inputArray[i][4]);
+  const r2 = parseInt(inputArray[i][5]);
 
   const sumR = r1 + r2;
   const diffR = Math.abs(r1, r2);
