f74e2c7c1198e78680494a98c247ff996f4ba1a2
Lee SeEun <sally4405@naver.com>
2020-08-03 23:44:45 +0900

    refactor: 프로그래머스 - 짝지어 제거하기 개선

--- Programmers/_짝지어 제거하기.js
+++ Programmers/_짝지어 제거하기.js
@@ -2,18 +2,35 @@
 
 let answer = 0;
 
+// const solution = (s) => {
+//   for (let i = 1; i < s.length; i++) {
+//     if (s[i - 1] === s[i]) {
+//       const sliced = s.slice(0, i - 1) + s.slice(i + 1);
+//       if (sliced) {
+//         solution(sliced);
+//         break;
+//       } else {
+//         answer = 1;
+//         return;
+//       }
+//     }
+//   }
+//   return answer;
+// };
+
 const solution = (s) => {
+  let sliced = s;
   for (let i = 1; i < s.length; i++) {
     if (s[i - 1] === s[i]) {
-      const sliced = s.slice(0, i - 1) + s.slice(i + 1);
-      if (sliced) {
-        solution(sliced);
-        break;
-      } else {
-        answer = 1;
-        return;
-      }
+      sliced = s.slice(0, i - 1) + s.slice(i + 1);
     }
+  }
+  if (sliced === s) return answer;
+  if (sliced) {
+    solution(sliced);
+  } else {
+    answer = 1;
+    return;
   }
   return answer;
 };
