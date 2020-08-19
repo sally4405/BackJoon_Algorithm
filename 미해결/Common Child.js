e5217dc2ae7a8fdf3b2b368753887577f76f4c15
Lee SeEun <sally4405@naver.com>
2020-08-19 23:58:27 +0900

    feat: hackerRank - common childe
    - 재귀로 풀면 될듯

--- 미해결/Common Child.js
+++ 미해결/Common Child.js
@@ -1,1 +1,23 @@
 // https://www.hackerrank.com/challenges/common-child/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+let answer = 0;
+function commonChild(s1, s2) {
+  loop(s1, s2);
+
+  return answer;
+}
+
+const loop = (s1, s2) => {
+  for (let i = 0; i < s1.length; i++) {
+    let tempAnswer = 0;
+    const compare = s[i];
+    for (let j = 0; j < s2.length; j++) {
+      if (s2[j] === compare) tempAnswer++;
+    }
+  }
+};
+
+const s1 = SHINCHAN;
+const s2 = NOHARAAA;
+
+console.log(commonChild(s1, s2));
