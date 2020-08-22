e8b9f13a27e4281e15b610b70b21fc10eedee73a
Lee SeEun <sally4405@naver.com>
2020-08-22 23:58:10 +0900

    feat: hackerRank - common child 테스트 케이스 추가

--- 미해결/Common Child.js
+++ 미해결/Common Child.js
@@ -2,22 +2,37 @@
 
 let answer = 0;
 function commonChild(s1, s2) {
-  loop(s1, s2);
+  loop(s1, s2, 0);
 
   return answer;
 }
 
-const loop = (s1, s2) => {
+const loop = (s1, s2, tempAnswer) => {
+  if (!s1.length || !s2.length) answer = answer > tempAnswer ? answer : tempAnswer;
   for (let i = 0; i < s1.length; i++) {
-    let tempAnswer = 0;
     const compare = s[i];
     for (let j = 0; j < s2.length; j++) {
-      if (s2[j] === compare) tempAnswer++;
+      if (s2[j] === compare) {
+        loop(s1.substring(i), s2.substring(j), tempAnswer + 1);
+      }
     }
   }
 };
 
-const s1 = SHINCHAN;
-const s2 = NOHARAAA;
+const s1 = HARRY;
+const s2 = SALLY;
+// 2
+
+// const s1 = AA
+// const s2 = BB
+// // 0
+
+// const s1 = SHINCHAN;
+// const s2 = NOHARAAA;
+// // 3
+
+// const s1 = ABCDEF
+// const s2 = FBDAMN
+// // 2
 
 console.log(commonChild(s1, s2));
