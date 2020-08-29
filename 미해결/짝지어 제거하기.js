da5e71043191493d1cb77cd819c445651d3dbecc
Lee SeEun <sally4405@naver.com>
2020-08-29 21:05:57 +0900

    programmers - 짝지어 제거하기 : 새로운 풀이 추가

--- 미해결/짝지어 제거하기.js
+++ 미해결/짝지어 제거하기.js
@@ -2,29 +2,19 @@
 
 let answer = 0;
 
-// const solution = (s) => {
-//   for (let i = 1; i < s.length; i++) {
-//     if (s[i - 1] === s[i]) {
-//       const sliced = s.slice(0, i - 1) + s.slice(i + 1);
-//       if (sliced) {
-//         solution(sliced);
-//         break;
-//       } else {
-//         answer = 1;
-//         return;
-//       }
-//     }
-//   }
-//   return answer;
-// };
+// 정확성 6,7,8,9,12,13 통과 (25.4)
 
 const solution = (s) => {
   let sliced = s;
+  let removeCount = 0;
   for (let i = 1; i < s.length; i++) {
     if (s[i - 1] === s[i]) {
-      sliced = s.slice(0, i - 1) + s.slice(i + 1);
+      sliced = sliced.slice(0, i - 1 - removeCount * 2) + sliced.slice(i + 1 - removeCount * 2);
+      removeCount++;
+      // i++;   // 정확성 1,9,10,11,12,13 통과 (25.4)
     }
   }
+
   if (sliced === s) return answer;
   if (sliced) {
     solution(sliced);
@@ -35,5 +25,32 @@ const solution = (s) => {
   return answer;
 };
 
-const s = "baabaa";
+// 정확성 1,9,10,11,12,13 통과 (25.4)
+
+// const solution = (s) => {
+//   let sliced = s;
+//   for (let i = 1; i < s.length; i++) {
+//     if (s[i - 1] === s[i]) {
+//       sliced = s.slice(0, i - 1) + s.slice(i + 1);
+//     }
+//   }
+//   if (sliced === s) return answer;
+//   if (sliced) {
+//     solution(sliced);
+//   } else {
+//     answer = 1;
+//     return;
+//   }
+//   return answer;
+// };
+
+const s = "acabaababbcaaa";
+// 1
+
+// const s = "baabaa";
+// // 1
+
+// const s = "cdcd";
+// // 0
+
 console.log(solution(s));
