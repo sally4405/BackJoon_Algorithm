3a40aac29c2d95d25be27b83f3ab7c26cbddca19
Lee SeEun <sally4405@naver.com>
2020-09-01 19:01:10 +0900

    backjoon - 2562: 구구단

---
+++ BackJoon/2739.js
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/2739
+// 구구단
+
+// const input = parseInt(require("fs").readFileSync("/dev/stdin").toString());
+const input = 2;
+
+for (let i = 1; i < 10; i++) {
+  console.log(`${input} * ${i} = ${input * i}`);
+}
