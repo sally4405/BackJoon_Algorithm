64ba8ffd541744c73b0782aed9a657a1c8a10f05
Lee SeEun <sally4405@naver.com>
2020-09-03 00:40:39 +0900

    backjoon - 1002: 터렛

--- BackJoon/1002.js
+++ BackJoon/1002.js
@@ -1,29 +1,22 @@
 // https://www.acmicpc.net/problem/1002
 // 터렛
 
+// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
 // const input = [3, 0, 0, 13, 40, 0, 37, 0, 0, 3, 0, 7, 4, 1, 1, 1, 1, 1, 5];
 // const input = [8, 0, 0, 1, 0, 4, 5, 0, 0, 1, 0, 4, 3, 0, 0, 1, 0, 4, 2, 0, 0, 1, 0, 4, 6, 0, 0, 1, 0, 4, 5];
 // const input = [1, 1, 1, 1, 1, 1, 5];
 // const input = [2, 0, 0, 1, 0, 0, 1, 0, 0, 10, 0, 0, 1];
-const raw = "3 \n0 0 13 40 0 37 \n0 0 3 0 7 4 \n1 1 1 1 1 5";
-const inputArray = raw
-  .toString()
-  .split("\n")
-  .map((n) => n.split(" "));
-// const inputArray = require("fs")
-//   .readFileSync("/dev/stdin")
-//   .toString()
-//   .split("\n")
-//   .map((n) => n.split(" "));
-
-console.log(inputArray);
+const input = "3\n0 0 13 40 0 37\n0 0 3 0 7 4\n1 1 1 1 1 5\n".split("\n");
+input.pop();
+const testCase = parseInt(input.shift());
+const inputArray = input.map((n) => n.split(" ").map((n) => parseInt(n)));
 
 const getDistance = (x1, y1, x2, y2) => {
-  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+  return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
 };
 
-const testCase = inputArray[0][0];
-for (let i = 1; i <= testCase; i++) {
+for (let i = 0; i < testCase; i++) {
   const x1 = parseInt(inputArray[i][0]);
   const y1 = parseInt(inputArray[i][1]);
   const r1 = parseInt(inputArray[i][2]);
@@ -31,8 +24,8 @@ for (let i = 1; i <= testCase; i++) {
   const y2 = parseInt(inputArray[i][4]);
   const r2 = parseInt(inputArray[i][5]);
 
-  const sumR = r1 + r2;
-  const diffR = Math.abs(r1, r2);
+  const sumR = Math.pow(r1 + r2, 2);
+  const diffR = Math.pow(Math.abs(r1 - r2), 2);
 
   const d = getDistance(x1, y1, x2, y2);
 
