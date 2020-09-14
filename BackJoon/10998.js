821f8227a24a701b6da36b43d0e7551238913520
Lee SeEun <sally4405@naver.com>
2020-09-14 23:54:33 +0900

    backjoon - 10998: AxB

---
+++ BackJoon/10998.js
@@ -0,0 +1,6 @@
+// https://www.acmicpc.net/problem/10998
+// A×B
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().split(" ").map((n) => parseInt(n));
+const input = "1 2".split(" ").map((n) => parseInt(n));
+console.log(input[0] * input[1]);
