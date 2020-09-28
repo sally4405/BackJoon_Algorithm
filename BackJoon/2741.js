37dc26901651b95960833f5ad0b729d4636b7516
Lee SeEun <sally4405@naver.com>
2020-09-28 23:58:56 +0900

    backjoon - 2741: N 찍기

---
+++ BackJoon/2741.js
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/2741
+// N 찍기
+
+// const input = parseInt(require("fs").readFileSync("/dev/stdin").toString().split("\n")[0]);
+const input = parseInt("5\n".split("\n")[0]);
+
+for (let i = 0; i < input; i++) {
+  console.log(i + 1);
+}
