9818055894f66b0daa352f2fec9d1b5f2e9a5055
sally4405 <sally4405@naver.com>
2020-11-02 14:41:13 +0900

    backjoon - 8393: 합

---
+++ BackJoon/8393.js
@@ -0,0 +1,7 @@
+// https://www.acmicpc.net/problem/8393
+// 합
+
+// const input = parseInt(require("fs").readFileSync("/dev/stdin").toString());
+const input = parseInt("3\n");
+
+console.log((input * (input + 1)) / 2);
