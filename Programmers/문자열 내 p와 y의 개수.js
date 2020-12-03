e2fc889f2c73dedcbab60b87cc7e11665dc0ff92
sally4405 <sally4405@naver.com>
2020-12-03 21:24:51 +0900

    프로그래머스 - 문자열 내 p와 y의 개수(수정)

--- Programmers/문자열 내 p와 y의 개수.js
+++ Programmers/문자열 내 p와 y의 개수.js
@@ -1,8 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12916
 
 function solution(s) {
-  var answer = true;
-
   let pNum = 0;
   let yNum = 0;
 
@@ -21,7 +19,7 @@ function solution(s) {
     }
   });
 
-  return answer;
+  return pNum === yNum;
 }
 
 const s = "pPoooyY";
