5054dbf8444f1a881486778b97d52be59ee3f2d8
sally4405 <sally4405@naver.com>
2020-12-11 04:54:22 +0900

    프로그래머스 - 문자열 다루기 기본 (수정)

--- Programmers/문자열 다루기 기본.js
+++ Programmers/문자열 다루기 기본.js
@@ -1,11 +1,11 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12918
 
 function solution(s) {
-  return (s.length === 4 || s.length === 6) && !Number.isNaN(parseInt(s));
+  return (s.length === 4 || s.length === 6) && !s.split("").filter((v) => Number.isNaN(parseInt(v))).length;
 }
 
 // const s = "a234";
 // const s = "1234";
-const s = "0234";
+const s = "02345a";
 
 console.log(solution(s));
