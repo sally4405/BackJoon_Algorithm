fb1c64b46ea13afff25a12a13514c2d775a7b71c
sally4405 <sally4405@naver.com>
2020-12-20 20:44:18 +0900

    프로그래머스 - x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,14 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12954
+
+function solution(x, n) {
+  var answer = [];
+  for (let i = 1; i <= n; i++) {
+    answer.push(x * i);
+  }
+  return answer;
+}
+
+const x = 2;
+const n = 5;
+
+console.log(solution(x, n));
