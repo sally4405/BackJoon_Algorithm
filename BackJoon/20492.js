466e5632f2cd4dee4f979bfc02fd6cc5addd70d7
sally4405 <sally4405@naver.com>
2021-01-01 13:23:45 +0900

    backjoon - 20492: 세금

---
+++ BackJoon/20492.js
@@ -0,0 +1,7 @@
+// https://www.acmicpc.net/problem/20492
+// 세금
+
+// const N = parseInt(require("fs").readFileSync("/dev/stdin").toString());
+const N = parseInt("10000000");
+
+console.log(`${N * 0.78} ${N * 0.956}`);
