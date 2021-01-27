3a24dcc554465d6a9c271fdafaf0d26afe9eb81a
sally4405 <sally4405@naver.com>
2021-01-27 23:51:16 +0900

    프로그래머스 - 모의고사 (수정)

--- Programmers/모의고사.js
+++ Programmers/모의고사.js
@@ -1,61 +1,21 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42840
+// [참고](https://programmers.co.kr/questions/15482)
 
 function solution(answers) {
-  var answer = [0, 0, 0];
+  var result = [0, 0, 0];
+
+  const p1 = [1, 2, 3, 4, 5];
+  const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
   for (let i = 0; i < answers.length; i++) {
-    if (answers[i] === i + 1) answer[0]++;
-
-    // if ((i + 1) % 2) {
-    //   second = second === 1 ? 3 : second === 5 ? 1 : second + 1;
-    //   if (answers[i] === 2) {
-    //     answer[1]++;
-    //   }
-    // } else if (second === answers[i]) answer[1]++;
-
-    if ((i + 1) % 2 && answers[i] === 2) answer[1]++;
-    else {
-      switch ((i + 1) % 8) {
-        case 2:
-          if (answers[i] === 1) answer[1]++;
-          break;
-        case 4:
-          if (answers[i] === 3) answer[1]++;
-          break;
-        case 6:
-          if (answers[i] === 4) answer[1]++;
-          break;
-        case 0:
-          if (answers[i] === 5) answer[1]++;
-          break;
-        default:
-          break;
-      }
-    }
-
-    switch (Math.ceil((i + 1) / 2) % 5) {
-      case 1:
-        if (answers[i] === 3) answer[2]++;
-        break;
-      case 2:
-        if (answers[i] === 1) answer[2]++;
-        break;
-      case 3:
-        if (answers[i] === 2) answer[2]++;
-        break;
-      case 4:
-        if (answers[i] === 4) answer[2]++;
-        break;
-      case 0:
-        if (answers[i] === 5) answer[2]++;
-        break;
-      default:
-        break;
-    }
+    if (answers[i] === p1[i % 5]) result[0]++;
+    if (answers[i] === p2[i % 8]) result[1]++;
+    if (answers[i] === p3[i % 10]) result[2]++;
   }
-  const max = Math.max(...answer);
+  const max = Math.max(...result);
 
-  return answer
+  return result
     .map((v, i) => {
       if (v === max) return i + 1;
     })
