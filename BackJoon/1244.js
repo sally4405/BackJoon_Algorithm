0b51d0ba82ef4e0c6fced2a45f23e7c0b0799f39
sally4405 <sally4405@naver.com>
2021-02-05 13:14:27 +0900

    backjoon - 1244: 스위치 켜고 끄기 (수정)

--- BackJoon/1244.js
+++ BackJoon/1244.js
@@ -1,20 +1,14 @@
 // https://www.acmicpc.net/problem/1244
 // 스위치 켜고 끄기
+// [참고](https://3dpit.tistory.com/31)
 
 // const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-// const input = "8\n0 1 0 1 0 0 0 1\n2\n1 3\n2 3\n".split("\n");
-const input = "21\n0 1 0 1 0 0 0 1 0 0 0 1 0 1 0 0 0 1 0 0 1\n2\n1 8\n2 7\n".split("\n");
+const input = "8\n0 1 0 1 0 0 0 1\n2\n1 3\n2 3\n".split("\n");
+// const input = "21\n0 1 0 1 0 0 0 1 0 0 0 1 0 1 0 0 0 1 0 0 1\n2\n1 8\n2 7\n".split("\n");
+// const input = "4\n0 1 0 1\n1\n2 3\n".split("\n");
 
 const N = input.shift();
-
-const state = input
-  .shift()
-  .split(" ")
-  .map((n) => {
-    if (n === "1") return true;
-    return false;
-  });
-
+const state = input.shift().split(" ");
 const studentN = input.shift();
 
 for (let i = 0; i < studentN; i++) {
@@ -22,33 +16,29 @@ for (let i = 0; i < studentN; i++) {
   let target = parseInt(switchNum);
 
   if (gender === "1") {
-    for (let j = 2; j < N; j++) {
-      state[target - 1] = !state[target - 1];
-      target *= j;
-      if (target > N) break;
+    for (let j = target; j <= N; j += target) {
+      state[j - 1] = state[j - 1] === "0" ? "1" : "0";
     }
   } else {
-    state[target - 1] = !state[target - 1];
-    let k = 1;
+    let lIdx = target;
+    let rIdx = target;
     while (true) {
-      if (state[target - 1 - k] !== state[target - 1 + k]) break;
-      state[target - 1 - k] = !state[target - 1 - k];
-      state[target - 1 + k] = !state[target - 1 + k];
-      k++;
-      if (state[target - 1 - k] === undefined || state[target - 1 + k] === undefined) break;
+      if (lIdx < 1 || rIdx > N + 1) break;
+      if (state[lIdx - 1] !== state[rIdx - 1]) break;
+      lIdx--;
+      rIdx++;
+    }
+    lIdx++;
+    rIdx--;
+    for (; lIdx <= rIdx; lIdx++) {
+      state[lIdx - 1] = state[lIdx - 1] === "0" ? "1" : "0";
     }
   }
 }
 
 let result = "";
 for (let m = 0; m < Math.ceil(N / 20); m++) {
-  result += `${state
-    .slice(m * 20, (m + 1) * 20)
-    .map((n) => {
-      if (n) return 1;
-      return 0;
-    })
-    .join(" ")}\n`;
+  result += `${state.slice(m * 20, (m + 1) * 20).join(" ")}\n`;
 }
 
 console.log(result);
