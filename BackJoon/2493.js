4446fb3074c77e8ef0648e52d85daa0376792224
sally4405 <sally4405@naver.com>
2021-02-06 23:24:51 +0900

    backjoon - 2493: 탑

---
+++ BackJoon/2493.js
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/2493
+// 탑
+// [참고](https://cocoon1787.tistory.com/43)
+
+// const [N, input] = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const [N, input] = "5\n6 9 5 7 4\n".split("\n");
+
+const tower = input.split(" ");
+const stack = [];
+const result = [];
+
+for (let i = 0; i < N; i++) {
+  while (stack.length) {
+    if (stack[stack.length - 1][1] > parseInt(tower[i])) {
+      result.push(stack[stack.length - 1][0] + 1);
+      break;
+    }
+    stack.pop();
+  }
+  if (!stack.length) result.push(0);
+  stack.push([i, parseInt(tower[i])]);
+}
+
+console.log(result.join(" "));
