0925cdfedfb086053ece77c3cb792b77af669146
sally4405 <sally4405@naver.com>
2021-02-16 17:34:32 +0900

    backjoon - 2108: 통계학(수정)
    - map 을 이용하여 풀이

--- BackJoon/2108.js
+++ BackJoon/2108.js
@@ -1,6 +1,8 @@
 // https://www.acmicpc.net/problem/2108
 // 통계학
 
+// [참고](https://dpsc615.tistory.com/132)
+
 // const [N, ...input] = require("fs").readFileSync("/dev/stdin").toString().split("\n").map((n) => parseInt(n));
 const [N, ...input] = "5\n1\n3\n8\n-2\n2\n".split("\n").map((n) => parseInt(n));
 // const [N, ...input] = "5\n-1\n-2\n-3\n-1\n-2\n".split("\n").map((n) => parseInt(n));
@@ -10,25 +12,24 @@ input.pop();
 input.sort((a, b) => a - b);
 
 let sum = 0;
-let most = [];
+let most = {};
 const min = input[0];
 
 input.map((n) => {
   sum += n;
-  if (most[n - min]) most[n - min]++;
-  else most[n - min] = 1;
+  if (most[n]) most[n]++;
+  else most[n] = 1;
 });
 
-// const mm = Math.max(...most.filter((v) => v));
-const mm = Math.max.apply(
-  null,
-  most.filter((v) => v)
-);
-const freq = most
-  .map((v, i) => {
-    if (v === mm) return i + min;
-  })
-  .filter((v) => v);
+const mm = Math.max.apply(null, Object.values(most));
+
+const freq = [];
+for (let key in most) {
+  if (most[key] === mm) {
+    freq.push(key);
+  }
+}
+freq.sort((a, b) => a - b);
 
 console.log(Math.round(sum / N));
 console.log(input[Math.floor(N / 2)]);
