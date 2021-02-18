d319ccb97fd5efadb782fd10a06ded748c20170a
sally4405 <sally4405@naver.com>
2021-02-18 21:49:30 +0900

    backjoon - 1929: 소수 구하기(수정 완료)

--- BackJoon/1929.js
+++ BackJoon/1929.js
@@ -1,25 +1,27 @@
 // https://www.acmicpc.net/problem/1929
 // 소수 구하기
 
-const start = new Date().getTime();
+// [참고](https://dpsc615.tistory.com/105)
 
-// const [N, M] = require("fs").readFileSync("/dev/stdin").toString().split(" ").map((n) => parseInt(n));
-// const [M, N] = "3 16\n".split(" ").map((n) => parseInt(n));
-const [M, N] = "1 1000000\n".split(" ").map((n) => parseInt(n));
-let answer = "";
+// const [M, N] = require("fs").readFileSync("/dev/stdin").toString().split(" ").map((n) => parseInt(n));
+const [M, N] = "2 3\n".split(" ").map((n) => parseInt(n));
+// const [M, N] = "1 1000000\n".split(" ").map((n) => parseInt(n));
 
-for (let i = M; i <= N; i++) {
-  let isPrime = true;
-  for (let j = 2; j < i; j++) {
-    if (!(i % j)) {
-      isPrime = false;
-      break;
+const isPrimeArr = Array(N + 1).fill(true);
+isPrimeArr[0] = isPrimeArr[1] = 0;
+
+for (let i = 2; i < N + 1; i++) {
+  if (isPrimeArr[i]) {
+    for (let square = Math.pow(i, 2); square < N + 1; square += i) {
+      isPrimeArr[square] = false;
     }
   }
-  if (isPrime) answer += `${i}\n`;
 }
 
-console.log(answer.length);
+let answer = "";
 
-const end = new Date().getTime() - start;
-console.log("소요된 시간: " + end + "ms");
+for (let i = M; i < N + 1; i++) {
+  if (isPrimeArr[i]) answer += `${i}\n`;
+}
+
+console.log(answer);
