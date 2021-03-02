383860864b118c75352fa1e0ad73c873387c45ee
sally4405 <sally4405@naver.com>
2021-03-03 00:07:30 +0900

    프로그래머스 - 구명보트 (효율성 테스트 통과)

--- Programmers/구명보트.js
+++ Programmers/구명보트.js
@@ -4,19 +4,18 @@ function solution(people, limit) {
   var answer = 0;
   people.sort((a, b) => b - a);
 
-  while (people.length) {
-    const target = people.shift();
-    if (target <= limit / 2) {
-      answer += Math.ceil((people.length + 1) / 2);
+  let lIdx = 0;
+  let rIdx = people.length - 1;
+
+  while (lIdx <= rIdx) {
+    if (people[lIdx] <= limit / 2) {
+      answer += Math.ceil((rIdx - lIdx + 1) / 2);
       break;
     }
 
     answer++;
-
-    if (target + people[people.length - 1] <= limit) {
-      people.pop();
-      continue;
-    }
+    rIdx = people[lIdx] + people[rIdx] <= limit ? rIdx - 1 : rIdx;
+    lIdx++;
   }
 
   return answer;
@@ -25,5 +24,6 @@ function solution(people, limit) {
 // const people = [70, 50, 80, 50];
 const people = [70, 80, 50];
 const limit = 100;
+// return 3
 
 console.log(solution(people, limit));
