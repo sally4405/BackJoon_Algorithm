1944233ba3fd979b795e75b313dbb751fa274225
sally4405 <sally4405@naver.com>
2021-04-14 16:37:34 +0900

    프로그래머스 - 순위 검색 (효율성 테스트 실패)
    - 효율성 증가 시켰으나 실패

--- Programmers/순위 검색.js
+++ Programmers/순위 검색.js
@@ -1,24 +1,62 @@
 // https://programmers.co.kr/learn/courses/30/lessons/72412
 
 function solution(info, query) {
-  var answer = Array(query.length).fill(0);
+  var answer = [];
+  const infoMap = new Map();
 
-  const infoMap = info.map((n) => {
-    const target = n.split(" ");
-    return { language: target[0], end: target[1], age: target[2], food: target[3], score: parseInt(target[4]) };
-  });
+  for (let i = 0; i < info.length; i++) {
+    cutNpush2Map(infoMap, info[i]);
+  }
+
+  for (let value of infoMap.values()) {
+    value.sort((a, b) => b - a);
+  }
 
   for (let i = 0; i < query.length; i++) {
-    const target = query[i].split(" ").filter((v) => v !== "and");
-    infoMap.map((n) => {
-      if ((target[0] === "-" || n.language === target[0]) && (target[1] === "-" || n.end === target[1]) && (target[2] === "-" || n.age === target[2]) && (target[3] === "-" || n.food === target[3]) && n.score >= parseInt(target[4])) {
-        answer[i]++;
-      }
-    });
+    const [language, end, age, foodScore] = query[i].split(" and ");
+    const [food, score] = foodScore.split(" ");
+
+    const target = infoMap.get(`${language}${end}${age}${food}`);
+    answer.push(binarySearch(target, parseInt(score)));
   }
 
   return answer;
 }
+
+const cutNpush2Map = (map, str) => {
+  const [language, end, age, food, score] = str.split(" ");
+
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 2; j++) {
+      for (let k = 0; k < 2; k++) {
+        for (let m = 0; m < 2; m++) {
+          const target = `${i ? language : "-"}${j ? end : "-"}${k ? age : "-"}${m ? food : "-"}`;
+          if (!map.has(target)) map.set(target, []);
+          map.set(target, [...map.get(target), parseInt(score)]);
+        }
+      }
+    }
+  }
+};
+
+const binarySearch = (sortedArray, seekElement) => {
+  if (!sortedArray) return 0;
+
+  let startIndex = 0;
+  let endIndex = sortedArray.length - 1;
+
+  while (startIndex <= endIndex) {
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+    if (sortedArray[middleIndex] >= seekElement) {
+      startIndex = middleIndex + 1;
+    } else {
+      endIndex = middleIndex - 1;
+    }
+  }
+
+  return startIndex;
+};
 
 const info = ["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"];
 const query = ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"];
