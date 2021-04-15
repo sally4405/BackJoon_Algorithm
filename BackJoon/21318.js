61e2e6624b0d41aff9f603ddef40a92188098905
sally4405 <sally4405@naver.com>
2021-04-16 02:29:42 +0900

    backjoon - 21318: 피아노 체조
    - x, y 둘 다 1인 경우 고려하여 해결

--- BackJoon/21318.js
+++ BackJoon/21318.js
@@ -2,7 +2,8 @@
 // 피아노 체조
 
 // const [N, difficulty, Q, ...input] = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-const [N, difficulty, Q, ...input] = "9\n1 2 3 3 4 1 10 8 1\n5\n1 3\n2 5\n4 7\n9 9\n5 9\n".split("\n");
+const [N, difficulty, Q, ...input] = "10\n1 2 3 3 4 1 10 8 1\n5\n1 1\n1 3\n2 5\n4 7\n9 9\n5 9\n".split("\n");
+// const [N, difficulty, Q, ...input] = "9\n1 2 3 3 4 1 10 8 1\n5\n1 3\n2 5\n4 7\n9 9\n5 9\n".split("\n");
 // const [N, difficulty, Q, ...input] = "6\n573 33283 5572 346 906 567\n5\n5 6\n1 3\n2 2\n1 6\n3 6\n".split("\n");
 
 const diff = difficulty.split(" ").map((n) => parseInt(n));
@@ -17,7 +18,7 @@ for (let i = 0; i < N; i++) {
 
 for (let i = 0; i < Q; i++) {
   const [x, y] = input[i].split(" ").map((n) => parseInt(n));
-  answer += `${miss[y - 2] - (miss[x - 2] ? miss[x - 2] : 0)}\n`;
+  answer += `${(miss[y - 2] ? miss[y - 2] : 0) - (miss[x - 2] ? miss[x - 2] : 0)}\n`;
 }
 
 console.log(answer);
