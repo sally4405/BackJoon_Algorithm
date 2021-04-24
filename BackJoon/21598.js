a8855f78bc4f4a8f48aeb8a518f9ef370486ba76
sally4405 <sally4405@naver.com>
2021-04-25 00:23:58 +0900

    backjoon - 21598: SciComLove

---
+++ BackJoon/21598.js
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/21598
+// SciComLove
+
+// const N = require("fs").readFileSync("/dev/stdin").toString();
+const N = "2";
+
+let answer = "";
+for (let i = 0; i < N; i++) {
+  answer += "SciComLove\n";
+}
+
+console.log(answer);
