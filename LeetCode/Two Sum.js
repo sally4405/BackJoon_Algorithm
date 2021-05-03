88d918b9ad6e89ea300851c73d47898876b5bd32
sally4405 <sally4405@naver.com>
2021-05-03 22:13:25 +0900

    LeetCode - Two Sum

---
+++ LeetCode/Two Sum.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/two-sum/
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) continue;
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+};
+
+// const nums = [2, 7, 11, 15];
+// const target = 9;
+
+const nums = [3, 2, 4];
+const target = 6;
+
+console.log(twoSum(nums, target));
