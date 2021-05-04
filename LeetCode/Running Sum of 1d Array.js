03c0583f1cf7f07b54a31f218552bad28aca7868
sally4405 <sally4405@naver.com>
2021-05-05 00:16:48 +0900

    LeetCode - Running Sum of 1d Array

---
+++ LeetCode/Running Sum of 1d Array.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/running-sum-of-1d-array/
+
+var runningSum = function (nums) {
+  const answer = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    answer.push(answer[i - 1] + nums[i]);
+  }
+  return answer;
+};
+
+const nums = [1, 2, 3, 4];
+
+console.log(runningSum(nums));
