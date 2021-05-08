61b084c4f95514d8b4a1e8638baa98f8672e4b09
sally4405 <sally4405@naver.com>
2021-05-08 22:43:09 +0900

    LeetCode - Sqrt(x)

---
+++ LeetCode/Sqrt(x).js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/sqrtx/
+
+var mySqrt = function (x) {
+  return Math.floor(Math.sqrt(x));
+};
+
+const x = 4;
+// 2
+
+// const x = 8;
+// // 2
+
+console.log(mySqrt(x));
