2d94083ee1552bf924fb9c5717df58b2845d1c3f
sally4405 <sally4405@naver.com>
2021-06-07 01:43:38 +0900

    baekjoon - 21918: 전구
    - for문의 범위가 둘 다 string인 경우 틀림
    - case 2,3,4의 b와 c 중 하나를 int 형으로 바꿔주어야 함
    - 왜 둘 다 string 이면 안되는지 모르겠음

--- BaekJoon/21918.js
+++ BaekJoon/21918.js
@@ -1,41 +1,31 @@
 // https://www.acmicpc.net/problem/21918
 // 전구
 
-const readline = require("readline");
+// const [NM, s, ...input] = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const [NM, s, ...input] = "8 3\n0 0 0 0 0 0 0 0\n1 2 1\n1 4 1\n2 2 4\n".split("\n");
+// const [NM, s, ...input] = "8 6\n0 0 0 0 0 0 0 0\n1 2 1\n1 4 1\n2 2 4\n2 1 7\n3 5 8\n4 4 6\n".split("\n");
+// const [NM, s, ...input] = "1 1\n1\n4 1 1\n".split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const [N, M] = NM.split(" ");
+const state = s.split(" ");
 
-let input = [];
-
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  const [N, M] = input[0].split(" ");
-  const state = input[1].split(" ");
-
-  for (let i = 2; i < parseInt(M) + 2; i++) {
-    const [a, b, c] = input[i].split(" ");
-    switch (a) {
-      case "1":
-        state[b - 1] = c;
-        break;
-      case "2":
-        for (let j = b; j <= c; j++) state[j - 1] = state[j - 1] === "0" ? "1" : "0";
-        break;
-      case "3":
-        for (let j = b; j <= c; j++) state[j - 1] = "0";
-        break;
-      case "4":
-        for (let j = b; j <= c; j++) state[j - 1] = "1";
-        break;
-      default:
-        break;
-    }
+for (let [a, b, c] of input.map((n) => n.split(" "))) {
+  switch (a) {
+    case "1":
+      state[b - 1] = c;
+      break;
+    case "2":
+      for (let j = b - 1; j <= c - 1; j++) state[j] = state[j] === "0" ? "1" : "0";
+      break;
+    case "3":
+      for (let j = b - 1; j <= c - 1; j++) state[j] = "0";
+      break;
+    case "4":
+      for (let j = b - 1; j <= c - 1; j++) state[j] = "1";
+      break;
+    default:
+      break;
   }
+}
 
-  console.log(state.join(" "));
-  process.exit();
-});
+console.log(state.join(" "));
