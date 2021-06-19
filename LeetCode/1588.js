5dc5140f7508533856c5c9d0950c61eed5145505
sally4405 <sally4405@naver.com>
2021-06-19 23:45:49 +0900

    LeetCode - 1588: Sum of All Odd Length Subarrays (개선)
    - 3중 for문에서 단일 for문 두개로 효율 개선

--- LeetCode/1588.js
+++ LeetCode/1588.js
@@ -2,31 +2,39 @@
 // Sum of All Odd Length Subarrays
 
 var sumOddLengthSubarrays = function (arr) {
-  let len = 1;
-  let answer = 0;
+  let nums = [];
+  const halfLen = Math.ceil(arr.length / 2);
+  let target = 0;
+  let numsR;
 
-  while (len <= arr.length) {
-    let idx = 0;
-    while (true) {
-      if (idx + len > arr.length) break;
-      for (let i = idx; i < idx + len; i++) {
-        answer += arr[i];
-      }
-      idx++;
+  if (arr.length % 2) {
+    for (let i = 0; i < halfLen; i++) {
+      target += halfLen - Math.ceil(i / 2) * 2;
+      nums.push(target);
     }
-    len += 2;
+    numsR = nums.slice(0, -1).reverse();
+  } else {
+    for (let i = 0; i < halfLen; i++) {
+      target += halfLen - i;
+      nums.push(target);
+    }
+    numsR = nums.concat().reverse();
   }
 
-  return answer;
+  nums = nums.concat(numsR);
+  return arr.reduce((acc, cur, i) => acc + cur * nums[i], 0);
 };
 
-// const arr = [1, 4, 2, 5, 3];
-// // 58
+const arr = [1, 4, 2, 5, 3];
+// 58
 
 // const arr = [1, 2];
 // // 3
 
-const arr = [10, 11, 12];
-// 66
+// const arr = [10, 11, 12];
+// // 66
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// const arr = [1, 2, 3, 4, 5];
 
 console.log(sumOddLengthSubarrays(arr));
