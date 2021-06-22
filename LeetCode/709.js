a82ab07b0ee2b58c7691dafc492344a68d2ddef8
sally4405 <sally4405@naver.com>
2021-06-22 16:33:07 +0900

    LeetCode - 709: To Lower Case

---
+++ LeetCode/709.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/to-lower-case/
+// To Lower Case
+
+var toLowerCase = function (s) {
+  return s.toLowerCase();
+};
+
+// const s = "Hello";
+// // Output: "hello"
+
+const s = "here";
+// Output: "here"
+
+// const s = "LOVELY";
+// // Output: "lovely"
+
+console.log(toLowerCase(s));
